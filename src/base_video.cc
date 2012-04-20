e44702a04c7d0b284df09c90583ac08b848f48b9
Daniil Bondarev <xonatius@gmail.com>
2012-04-20 23:14:28 +0300

    Added processPanelPart function. Now working with display panel

--- src/base_video.cc
+++ src/base_video.cc
@@ -17,27 +17,31 @@ const int V_DELAY = 33;
 char path[100] = "";
 int num=0;
 
-void lineSearchAdd(const Mat &src, Mat &dst)
+void processPanelPart(Mat binaryFrame, const PanelInfo &panelInfo, const int linePixelCount=10, const int linePixelGap=3)
 {
+    // Searching for vertical lines
     // TODO: Remove Hough line search add simplier iterative search
+    Mat panelPart = binaryFrame(panelInfo.panelRect), dst;
+    // Convert color to draw lines on debug output
+    cvtColor(panelPart, dst, CV_GRAY2BGR);
     vector<Vec4i> lines;
-    HoughLinesP(src, lines, 1, CV_PI/2, 10, 10, 5 );
-    for( size_t i = 0; i < lines.size(); i++ )
+    vector<Vec4i> verticalLines;
+    HoughLinesP(panelPart, lines, 1, CV_PI/2, linePixelCount, linePixelCount, linePixelGap);
+    for(size_t i = 0; i < lines.size(); i++)
     {
-        Vec4i l = lines[i];
+        Vec4i &l = lines[i];
         if (l[0] != l[2])
+        // Not vertical case:
             continue;
-        line( dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 1, CV_AA);
-        //cout << "Line: " << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << endl;
+        verticalLines.push_back(l);
+        // Debuging drawing
+        line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 1, CV_AA);
     }
+    // Debaging show
+    imshow(WIN_VIDEO, dst);
 }
 
-void processMainPart(const Mat &binaryFrame)
-{
-    
-}
-
-void processFrame(Mat &src, const PanelInfo &panelInfo)
+void processFrame(const Mat &src, const PanelInfo &panelInfo)
 {
     Mat dst, src_gray, tmp;
     // To make a binary mask changing src color to gray
@@ -46,10 +50,7 @@ void processFrame(Mat &src, const PanelInfo &panelInfo)
     rotateImage(src_gray, tmp, panelInfo.angle);
     // Using otsu threshold binarisation.
     adaptiveThreshold(tmp, dst, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 3, 3);
-    //// Removing one pixel noise
-    //cleanNoise(tmp, dst);
-    lineSearchAdd(dst, src);
-    imshow(WIN_VIDEO, dst);
+    processPanelPart(dst, panelInfo);
     //sprintf(path, "processed/step_2-rotation/frame_%.4d.png", num++);
     //imwrite(path, dst);
 }
